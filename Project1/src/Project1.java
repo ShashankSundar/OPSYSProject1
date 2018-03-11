7d208fc4846aa765f26ec5b995dca97cead6a5a1
chengj6 <chengj6@rpi.edu>
2018-03-11 15:50:05 -0400

    Making RR

--- Project1/src/Project1.java
+++ Project1/src/Project1.java
@@ -198,5 +198,127 @@ public class Project1 {
 		System.out.println("Preemptions: "+numPreemptions);
 	}
 	
+	private static void rr(ArrayList<Process> processes) {
+		int timeSlice = 80;
+		double avgWaitTime = 0.0;
+		double avgBurstTime = 0.0;
+		double avgTurnaroundTime = 0.0;
+		int numContextSwitches = 0;
+		int numPreemptions = 0; //constant: FCFS has no preemptions
+		int totalBursts = 0;
+		int n = processes.size();
+		int time = 0;
+		ArrayList<Process> queue = new ArrayList<>();
+		ArrayList<Process> ioBlock = new ArrayList<>();
+		Process currentProcess = null;
+		boolean done = false;
+		
+		System.out.println("time "+time+"ms: Simulator started for RR [Q <empty>]");
+		
+		while (!done) {
+			// processes arrive
+			for(int i = 0; i < processes.size(); i++) {
+				if (processes.get(i).getArrivalTime() == time) {
+					avgBurstTime += processes.get(i).getOriginalBurstTime() * processes.get(i).getOriginalBursts();
+					totalBursts += processes.get(i).getOriginalBursts();
+					queue.add(processes.get(i));
+					System.out.print("time "+time+"ms: Process "+processes.get(i).getID()+" arrived and added to ready queue");
+					printQueue(queue);
+				}
+			}
+			
+			// context switch in to start process
+			if (currentProcess == null && queue.size() > 0) {
+				currentProcess = queue.remove(0);
+				//context switch
+				for (int i = 0; i < T_CS/2; i++) {
+					time++;
+					ioHandle(time, queue, ioBlock);
+					waitingProc(queue);
+				}
+				System.out.print("time "+time+"ms: Process "+currentProcess.getID()+" started using the CPU");
+				printQueue(queue);	
+				continue;
+			}
+			
+			// process burst finishes
+			if (currentProcess != null && currentProcess.getRemainingBurstTime() -1 == 0) {
+				time++;
+				currentProcess.decrementBurst();
+				currentProcess.decrementNumBursts();
+				currentProcess.resetBurstTime();
+				if (currentProcess.getRemainingBursts() != 0) {
+					if (currentProcess.getRemainingBursts() == 1) 
+						System.out.print("time "+time+"ms: Process "+currentProcess.getID()+" completed a CPU burst; "+currentProcess.getRemainingBursts()
+								+ " burst to go");
+					else
+						System.out.print("time "+time+"ms: Process "+currentProcess.getID()+" completed a CPU burst; "+currentProcess.getRemainingBursts()
+						+ " bursts to go");
+					printQueue(queue);
+					System.out.print("time "+time+"ms: Process "+currentProcess.getID()+" switching out of CPU; will block on I/O until time "
+							+ (time+currentProcess.getRemainingIOTime() + T_CS/2)+"ms");
+					printQueue(queue);
+					ioHandle(time, queue, ioBlock);
+					//context switch
+					for (int i = 0; i < T_CS/2; i++) {
+						time++;
+						ioHandle(time, queue, ioBlock);
+						waitingProc(queue);
+					}
+					
+					Process temp = new Process(currentProcess);
+					ioBlock.add(temp);
+					currentProcess = null;
+					continue;
+				}
+				
+				System.out.print("time "+time+"ms: Process "+currentProcess.getID()+" terminated");
+				printQueue(queue);
+				avgWaitTime += currentProcess.getWaitTime();
+				avgTurnaroundTime += currentProcess.getWaitTime() + (time-currentProcess.getArrivalTime());
+				n--;
+				ioHandle(time, queue, ioBlock);
+				currentProcess = null;
+				for (int i = 0; i < T_CS/2; i++) {
+					time++;
+					ioHandle(time, queue, ioBlock);
+					waitingProc(queue);
+				}
+				continue;
+			}
+			
+			// all processes done
+			if (n == 0) 
+				break;
+						
+			time++;
+			
+			// I/0
+			ioHandle(time, queue, ioBlock);
+			
+			// Waiting
+			waitingProc(queue);
+			
+			
+			// decrement running process
+			if (currentProcess != null) {
+				currentProcess.decrementBurst();
+			}
+				
+		}
+		
+		
+		avgBurstTime = avgBurstTime/totalBursts;
+		avgBurstTime = (double)Math.round(avgBurstTime * 100d) / 100d;
+		avgWaitTime = avgWaitTime/totalBursts;
+		avgWaitTime = (double)Math.round(avgWaitTime * 100d) / 100d;
+		avgTurnaroundTime = avgTurnaroundTime/(totalBursts*processes.size());
+		avgTurnaroundTime = (double)Math.round(avgTurnaroundTime * 100d) / 100d;
+		System.out.println("time "+time+"ms: Simulator ended for FCFS");
+		System.out.println("Avg Burst Time: "+avgBurstTime+" ms");
+		System.out.println("Avg Wait Time: "+avgWaitTime+" ms");
+		System.out.println("Avg Turnaround Time: "+avgTurnaroundTime+" ms");
+		System.out.println("Preemptions: "+numPreemptions);
+	}
 }
 
